cea51f14c35ae41aa68f4d48b1ce2e5cac100388
joesBel <sen.jiang@myxxjs.com>
2017-03-15 09:33:19 +0800

    接口调试

---
+++ web/host/hostUrl.js
@@ -0,0 +1,4 @@
+/**
+ * Created by ASUS on 2017/3/14.
+ */
+var DataUrl="http://47.88.103.131:9001/api";
---
+++ web/module/login_module.js
@@ -0,0 +1,13 @@
+
+var Login_model = angular.module("log_dev", ['ui.router',"ngCookies","loginController","login_router","CommonService"]);
+
+Login_model.config(['$httpProvider',"$cookiesProvider",function($httpProvider,$cookiesProvider) {
+    $cookiesProvider.defaults = {
+        path: "/",
+        domain: "",//cookie 作用域， cookies只在这个域和其子域有效
+        expires:new Date(new Date().getTime()+5000),//有效时间
+        secure: true//该cookie只在安全连接中被提供
+    };
+}]);/**
+ * Created by ASUS on 2017/3/14.
+ */
