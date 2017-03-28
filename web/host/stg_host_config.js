8e9c106c0def8a42c9c82125d16bc8d718d320f7
lingxue <info@myxxjs.com>
2017-03-28 09:02:46 +0800

    update http service

--- web/host/stg_host_config.js
+++ web/host/stg_host_config.js
@@ -4,7 +4,7 @@
 var hostService=angular.module("hostService",[]);
 hostService.factory("$host",[],function () {
     var _this={
-        DataUrl:"http://47.88.103.131:9001/api"
+        api_url:"http://47.88.103.131:9001/api"
     };
     return _this;
 
