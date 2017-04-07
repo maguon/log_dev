c6982ab3f27efa4b31b7deb96f400e624e59f610
joesBel <sen.jiang@myxxjs.com>
2017-04-07 14:53:00 +0800

    公司配置页面over。

--- web/js/controller/users_controller.js
+++ web/js/controller/users_controller.js
@@ -6,8 +6,26 @@ userController.controller("usersController",function () {
 
 });
 userController.controller("operator_controller",["$basic","$host","$scope",function ($basic,$host,$scope) {
+
     var adminId=sessionStorage.getItem("userId");
+    $basic.get($host.api_url+"/admin/"+adminId+"/user").then(function (data) {
+        if(data.success==true){
+           // console.log(data)
+            $scope.operator=data.result;
+
+        }else {
+            swal(data.msg,"","error");
+        }
+    })
+
     $scope.newOperator=function () {
+        $scope.submitted = false;
+        $scope.new_userName="";
+        $scope.new_depId="";
+        $scope.user_phone="";
+        $scope.user_sex="";
+        $scope.user_password="";
+
         $(".modal").modal();
         $("#newOperator").modal("open")
         $basic.get($host.api_url+"/admin/"+adminId+"/department").then(function (data) {
@@ -19,6 +37,32 @@ userController.controller("operator_controller",["$basic","$host","$scope",funct
             }
         })
     };
+    $scope.submitForm=function (isValid) {
+        $scope.submitted=true;
+
+        if(isValid){
+            var sex_id=$(".sex").attr("sex");
+            $scope.user_sex=sex_id;
+
+            var obj={
+                userName:$scope.new_userName,
+                deptId:$scope.new_depId,
+                gender:$scope.user_sex,
+                mobile:$scope.user_phone,
+                password:$scope.user_password
+            };
+            $basic.post($host.api_url+"/admin/"+adminId+"/user",obj).then(function (data) {
+                if(data.success==true){
+                    swal("新增成功","","success");
+                }else {
+                    swal(data.msg,"","error");
+                }
+
+            })
+
+        }
+    }
+
 
 
 }]);
