5629aedc200c2f70eb80e9932912fe7b57282bde
joesBel <sen.jiang@myxxjs.com>
2017-05-18 10:03:16 +0800

    调整

--- web/js/controller/setting/setting_amend_vin_controller.js
+++ web/js/controller/setting/setting_amend_vin_controller.js
@@ -2,14 +2,43 @@
  * Created by ASUS on 2017/5/17.
  */
 var setting_amend_vin_controller=angular.module("setting_amend_vin_controller",[]);
-setting_amend_vin_controller.controller("setting_amend_vin_controller",["$scope","$basic","$host",function ($scope,$basic,$host){
+setting_amend_vin_controller.controller("setting_amend_vin_controller",["$scope","$basic","$config_variable","$host",function ($scope,$basic,$config_variable,$host){
     var admin=$basic.getSession($basic.USER_ID);
+    // 查询vin码
     $scope.demand_car=function ($iValid) {
         $scope.submitted=true;
         if($iValid&&$scope.demand_vin.length==17){
-            $basic.get($host.api_url+"/admin/"+admin+"/car?vin="+$scope.demand_vin).then(function (data) {
+            var obj={
+                vin:$scope.demand_vin,
+                active:1
+            };
+            $basic.get($host.api_url+"/admin/"+admin+"/car?"+$basic.objToUrl(obj)).then(function (data) {
                 if(data.success=true){
+                    $scope.car_details=data.result[0];
+                    $scope.vin= $scope.car_details.vin;
 
+                    for(var i in $config_variable.config_color){
+                        if($config_variable.config_color[i].colorId==$scope.car_details.colour){
+                            $scope.color=$config_variable.config_color[i].colorName;
+                        }
+                    }
+
+                }
+            })
+        }
+    }
+    
+    // 修改vin码
+    $scope.amend_vin=function (id) {
+        var obj={
+            "vin":$scope.vin
+        };
+        if($scope.vin.length==17){
+            $basic.put($host.api_url+"/admin/"+admin+"/car/"+id+"/vin",obj).then(function (data) {
+                if(data.success==true){
+
+                }else {
+                    swal(data.msg,"","error")
                 }
             })
         }
