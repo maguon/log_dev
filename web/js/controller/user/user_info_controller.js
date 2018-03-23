867157e2caef37b11109c9aba6c1c81d4004313d
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-03-23 10:58:57 +0800

    修改命名以及追加注释,格式化代码

--- web/js/controller/user/user_info_controller.js
+++ web/js/controller/user/user_info_controller.js
@@ -1,35 +1,40 @@
 /**
  * Created by ASUS on 2017/5/16.
+ * 主菜单：用户信息 控制器
  */
-app.controller("user_info_controller",["$scope","_basic","_config","_host",function ($scope,_basic,_config,_host) {
-    var userInfoItem=_config.userTypes;
-    $scope.userInfoArray=[];
-    function userInfo() {
-        _basic.get(_host.api_url+"/user").then(function (data) {
-            if(data.success==true){
-                $scope.userInfoList=data.result;
-                for(var i in userInfoItem){
+app.controller("user_info_controller", ["$scope", "_basic", "_config", "_host", function ($scope, _basic, _config, _host) {
+    var userInfoItem = _config.userTypes;
+    $scope.userInfoArray = [];
+
+    /**
+     * 取得用户信息列表。
+     */
+    function getUserInfoList() {
+        _basic.get(_host.api_url + "/user").then(function (data) {
+            if (data.success == true) {
+                $scope.userInfoList = data.result;
+                for (var i in userInfoItem) {
                     $scope.userInfoArray.push({
-                        user_type:userInfoItem[i].type,
-                        type_name:userInfoItem[i].name,
-                        userInfoListArray:[]
+                        user_type: userInfoItem[i].type,
+                        type_name: userInfoItem[i].name,
+                        userInfoListArray: []
                     });
                 }
-                for(var k in $scope.userInfoArray){
-                    for(var j in $scope.userInfoList){
-                    if($scope.userInfoList[j].type==$scope.userInfoArray[k].user_type&&$scope.userInfoList[j].status==1){
-                        $scope.userInfoArray[k].userInfoListArray.push({
-                            real_name:$scope.userInfoList[j].real_name,
-                            mobile:$scope.userInfoList[j].mobile,
-                            gender:$scope.userInfoList[j].gender,
-                        })
+                for (var k in $scope.userInfoArray) {
+                    for (var j in $scope.userInfoList) {
+                        if ($scope.userInfoList[j].type == $scope.userInfoArray[k].user_type && $scope.userInfoList[j].status == 1) {
+                            $scope.userInfoArray[k].userInfoListArray.push({
+                                real_name: $scope.userInfoList[j].real_name,
+                                mobile: $scope.userInfoList[j].mobile,
+                                gender: $scope.userInfoList[j].gender,
+                            })
+                        }
                     }
                 }
-            }
-            }else {
-                swal(data.msg,"","error")
+            } else {
+                swal(data.msg, "", "error")
             }
         });
     };
-    userInfo();
+    getUserInfoList();
 }]);
