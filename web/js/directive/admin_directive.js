5fcd7dd0d0585ea6f54cf890714b1c58ba12f1ac
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-03-26 15:28:47 +0800

    Header部，追加了【修改个人密码】ICON

--- web/js/directive/admin_directive.js
+++ web/js/directive/admin_directive.js
@@ -6,13 +6,57 @@ adminDirective.directive('header', function () {
         transclude: false,
         restrict: 'E',
         controller: function ($scope, $element, $rootScope, _basic,_host) {
+
+            // 管理者权限
             if (_basic.checkUser("99")) {
+
+                // 取得当前登录用户ID
+                var userId = _basic.getSession(_basic.USER_ID);
+
+                /**
+                 * 左侧导航栏
+                 */
                 $("#menu_link").sideNav({
                     menuWidth: 280, // Default is 300
                     edge: 'left', // Choose the horizontal origin
                     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
                 });
                 $('.collapsible').collapsible();
+
+                /**
+                 * 显示【修改个人密码】画面。
+                 */
+                $scope.openModifyPw = function () {
+                    $(".modal").modal();
+                    $("#user_modal").modal("open");
+                };
+
+                /**
+                 * 更新密码。
+                 * @param valid 是否有效
+                 */
+                $scope.updatePassword = function (valid) {
+                    $scope.submitted = true;
+                    if (valid && $scope.user_new_password == $scope.user_confirm_password) {
+                        var obj = {
+                            "originPassword": $scope.user_old_password,
+                            "newPassword": $scope.user_new_password
+                        };
+                        // TODO 管理员修改的话，API 是 PUT /admin/{adminId}/password ？？
+                        _basic.put(_host.api_url + "/user/" + userId + "/password", obj).then(function (data) {
+                            if (data.success == true) {
+                                swal("密码重置成功", "", "success");
+                                $("#user_modal").modal("close");
+                            } else {
+                                swal(data.msg, "", "error");
+                            }
+                        })
+                    }
+                };
+
+                /**
+                 * 注销账号
+                 */
                 $scope.logOut = function () {
                     swal({
                         title: "注销账号",
@@ -31,6 +75,7 @@ adminDirective.directive('header', function () {
                         window.location.href = '/admin_login.html';
                     });
                 };
+
                 _basic.setHeader(_basic.USER_TYPE, _basic.getSession(_basic.USER_TYPE));
                 _basic.setHeader(_basic.COMMON_AUTH_NAME,  _basic.getSession(_basic.COMMON_AUTH_NAME) );
                 _basic.get(_host.api_url + "/admin/" + _basic.getSession(_basic.USER_ID)).then(function (data) {
@@ -43,11 +88,9 @@ adminDirective.directive('header', function () {
                         swal(data.msg, "", "error");
                     }
                 });
-
             } else {
                 window.location="./admin_login.html"
             }
-
         }
     };
 });
