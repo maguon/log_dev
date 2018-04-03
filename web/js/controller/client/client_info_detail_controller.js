24f46dd797c3a3d4dce031c0114e9753814157ab
star920626 <jing.zhang@myxxjs.com>
2018-04-03 14:50:35 +0800

    add client

---
+++ web/js/controller/client/client_info_detail_controller.js
@@ -0,0 +1,6 @@
+/**
+ * Created by star on 2018/4/3.
+ * 委托方详情
+ */
+app.controller("client_info_detail_controller", ["$scope", "$rootScope", "_host", "_basic", "_config", "_baseService", function ($scope, $rootScope, _host, _basic, _config, _baseService) {
+}])
