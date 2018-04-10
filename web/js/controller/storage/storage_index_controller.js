95431ab0b99f5b68f2a8333c92b9de3bc0f5f267
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-04-10 08:43:11 +0800

    修改：主控面板 变量修改

--- web/js/controller/storage/storage_index_controller.js
+++ web/js/controller/storage/storage_index_controller.js
@@ -1,8 +1,14 @@
+/**
+ * 主菜单：主控面板 控制器
+ */
 app.controller("storage_index_controller", ['$rootScope', '$scope', "_host", '$location', '$q', "_basic",
     function ($rootScope, $scope, _host, $location, $q, _basic) {
+
+        // 当天日期
         var date = new Date();
         var nowDate = moment(date).format('YYYYMMDD');
-        $scope.storageAllStorage = 0;
+
+        // 库存总车辆,今日入库,今日出库 数据取得
         _basic.get(_host.api_url + "/storageCount?dateStart=" + nowDate + "&dateEnd=" + nowDate).then(function (data) {
             if (data.success == true && data.result.length > 0) {
                 $scope.storageIndexCount = data.result[0];
@@ -10,11 +16,16 @@ app.controller("storage_index_controller", ['$rootScope', '$scope', "_host", '$l
                 swal(data.msg, "", "error");
             }
         });
+
+        // 仓储总车位
+        $scope.storageAllStorage = 0;
+
+        // 明细，仓储总车位 取得
         _basic.get(_host.api_url + "/storageDate?dateStart=" + nowDate + "&dateEnd=" + nowDate).then(function (data) {
             if (data.success == true) {
                 $scope.storageIndexList = data.result;
                 for (var i in $scope.storageIndexList) {
-                    $scope.storageAllStorage = $scope.storageAllStorage + $scope.storageIndexList[i].col * $scope.storageIndexList[i].row
+                    $scope.storageAllStorage = $scope.storageAllStorage + $scope.storageIndexList[i].total_seats;
                 }
                 return $scope.storageAllStorage;
             } else {
