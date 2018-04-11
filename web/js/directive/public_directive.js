41a49ef6de5fe4dc485cb85d5d182bbcb232a333
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-04-11 09:15:05 +0800

    修改：调整代码格式

--- web/js/directive/public_directive.js
+++ web/js/directive/public_directive.js
@@ -5,12 +5,17 @@ publicDirective.directive('header', function () {
         replace: true,
         transclude: false,
         restrict: 'E',
-        controller: function ($scope, $element, $rootScope, _basic, _host) {
+        controller: function ($scope, $element, $rootScope, _basic, _host, _config) {
 
-            // 管理者权限
+            // 权限
+            var str_type = $element.attr("type");
+            if (_basic.getSession(_basic.USER_TYPE) == str_type) {
 
                 // 取得当前登录用户ID
                 var userId = _basic.getSession(_basic.USER_ID);
+
+                var userType = _basic.getSession(_basic.USER_TYPE);
+                var user_info_obj = _config.userTypes;
 
                 /**
                  * 左侧导航栏
@@ -19,46 +24,63 @@ publicDirective.directive('header', function () {
                     menuWidth: 280, // Default is 300
                     edge: 'left', // Choose the horizontal origin
                     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                    draggable: true // Choose whether you can drag to open on touch screens
                 });
                 $('.collapsible').collapsible();
 
-                /**
-                 * 注销账号
-                 */
-                $scope.logOut = function () {
-                    swal({
-                        title: "注销账号",
-                        text: "是否确认退出登录",
-                        type: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#DD6B55",
-                        confirmButtonText: "确认",
-                        cancelButtonText: "取消",
-                        closeOnConfirm: false
-                    }, function () {
-                        _basic.removeSession(_basic.COMMON_AUTH_NAME);
-                        _basic.removeSession(_basic.USER_ID);
-                        _basic.removeSession(_basic.USER_TYPE);
-                        _basic.removeSession(_basic.USER_NAME);
-                        window.location.href = '/common_login.html';
-                    });
-                };
+                // $scope.qrList = [];
+                // for (var i = 0; i < user_info_obj.length; i++) {
+                //     if(userType == user_info_obj[i].type){
+                //         $scope.qrList = user_info_obj[i].qr;
+                //         break;
+                //     }
+                // }
+                // console.log("qrList",$scope.qrList);
 
-                _basic.setHeader(_basic.USER_TYPE, _basic.getSession(_basic.USER_TYPE));
+                $scope.hasUserPortrait = false;
+
+                _basic.setHeader(_basic.USER_TYPE, userType);
                 _basic.setHeader(_basic.COMMON_AUTH_NAME, _basic.getSession(_basic.COMMON_AUTH_NAME));
 
-                _basic.get(_host.api_url + "/user/" + _basic.getSession(_basic.USER_ID)).then(function (data) {
-
-                    if (data.success == true && data.result.length > 0) {
-                        $scope.userName = data.result[0].user_name;
+                _basic.get(_host.api_url + "/user/" + userId).then(function (data) {
+                    // $(".shadeDowWrap").hide();
+                    if (data.success == true) {
+                        // if(data.result[0].avatar_image != null){
+                        //     $scope.hasUserPortrait = true;
+                        //     $scope.userImg = _host.file_url + '/image/' + data.result[0].avatar_image;
+                        // }
+                        // else{
+                        //     $scope.hasUserPortrait = false;
+                        // }
+                        // _basic.setSession(_basic.USER_IMG, data.result[0].avatar_image);
                         _basic.setSession(_basic.USER_NAME, $scope.userName);
                         _basic.setHeader(_basic.USER_NAME, $scope.userName);
+                        _basic.setSession(_basic.USER_NAME, data.result[0].real_name);
+                        var user = {
+                            id: userId,
+                            type: userType,
+                            name: data.result[0].real_name
+                        };
+                        // _socket.connectSocket(user);
+                        for (var i = 0; i < user_info_obj.length; i++) {
+                            if (user_info_obj[i].type == data.result[0].type) {
+                                $scope.userName = user_info_obj[i].name;
+                            }
+                        }
+                        $scope.realName = data.result[0].real_name;
+                        // MaterialAvatar(document.getElementsByClassName('nav-avatar'), {
+                        //     shape: 'circle',
+                        //     backgroundColor: '#4dd0e1',
+                        //     textColor: '#fff'
+                        // });
                     } else {
                         swal(data.msg, "", "error");
                     }
                 });
 
-
+            } else {
+                window.location = "./common_login.html"
+            }
 
             /**
              * 显示【修改个人密码】画面。
@@ -89,6 +111,29 @@ publicDirective.directive('header', function () {
                     })
                 }
             };
+
+            /**
+             * 注销账号
+             */
+            $scope.logOut = function () {
+                swal({
+                    title: "注销账号",
+                    text: "是否确认退出登录",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确认",
+                    cancelButtonText: "取消",
+                    closeOnConfirm: false
+                }, function () {
+                    _basic.removeSession(_basic.COMMON_AUTH_NAME);
+                    _basic.removeSession(_basic.USER_ID);
+                    _basic.removeSession(_basic.USER_TYPE);
+                    _basic.removeSession(_basic.USER_NAME);
+                    window.location.href = '/common_login.html';
+                });
+            };
+
         }
     };
 });
@@ -112,35 +157,35 @@ publicDirective.directive('percent', function () {
         link: function (scope, element, attr) {
             var val = Number.parseInt(attr.value);
             var total = Number.parseInt(attr.total);
-            var percentage = Number.parseInt((val*100/total));
+            var percentage = Number.parseInt((val * 100 / total));
             if (total != 0) {
                 percentage = Number.parseInt((val * 100 / total));
-            }else {
+            } else {
                 percentage = 0;
             }
             $(element[0].children[0]).highcharts({
                 // 表头
                 title: {
-                    text:percentage+"%",
+                    text: percentage + "%",
                     align: 'center',
                     verticalAlign: 'middle',
-                    y:8,
-                    style:{
-                        color:"#bdbdbd"
+                    y: 8,
+                    style: {
+                        color: "#bdbdbd"
                     }
                 },
-                colors:[
+                colors: [
                     "#4dd0e1",
                     "#cfd8dc"
                 ],
                 // 版权信息
                 credits: {
-                    enabled:"false",
+                    enabled: "false",
                     text: '',
                     href: ''
                 },
                 tooltip: {
-                    enabled : false
+                    enabled: false
                 },
                 plotOptions: {
                     pie: {
@@ -159,8 +204,8 @@ publicDirective.directive('percent', function () {
                     name: '',
                     innerSize: '80%',
                     data: [
-                        ['',   percentage],
-                        ['',   (100-percentage)]
+                        ['', percentage],
+                        ['', (100 - percentage)]
                     ]
                 }]
             });
@@ -270,13 +315,12 @@ publicDirective.directive("formDate", function () {
     }
 });
 
-
 // 验证数字(正数)的指令，只能输入[0-9]
 publicDirective.directive('validNum', ['$parse', function ($parse) {
     return {
-        restrict:"A",
+        restrict: "A",
         require: 'ngModel',
-        link:function(scope,element,attrs,ngModelCtrl){
+        link: function (scope, element, attrs, ngModelCtrl) {
             // make sure we're connected to a model
             if (!ngModelCtrl) {
                 return;
