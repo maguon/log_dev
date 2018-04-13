d62d4aa69cb5a6124fae3d9892386a371314d3b1
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-04-13 13:07:37 +0800

    新规：订单支付相关机能追加

--- web/js/controller/storage/storage_order_detail_controller.js
+++ web/js/controller/storage/storage_order_detail_controller.js
@@ -21,10 +21,18 @@ app.controller("storage_order_detail_controller", ["$scope", "$state", "$statePa
     // 是否显示关联订单详情部分（支付信息画面）
     $scope.otherOrderList = false;
 
+    // 关联仓储订单列表
+    $scope.relOrderList = [];
+
+    // 预计支付合计
+    $scope.totalPlanFee = 0;
+    // 实际应付合计
+    $scope.totalActualFee = 0;
+
     // 订单信息
     $scope.orderInfo = {
-        id: "",
         orderStatus:"",
+        entrustId: 0,
         entrustName: "",
         vin: "",
         makeName: "",
@@ -37,45 +45,26 @@ app.controller("storage_order_detail_controller", ["$scope", "$state", "$statePa
         actualFee: ""
     };
 
-    /**
-     * 获取钥匙柜分区信息列表
-     */
-    $scope.getOrderDetails = function () {
-
-        // 扇区名称
-        var url = _host.api_url + "/carKeyCabinetArea?carKeyCabinetId=" + keyCabinetId + "&areaId=" + $scope.selectedZone;
-
-        _basic.get(url).then(function (data) {
-            if (data.success == true) {
-                $scope.keyInfo.zoneNm = data.result[0].area_name;
-            } else {
-                swal(data.msg, "", "error");
-            }
-        });
-
-        // 行列
-        $scope.keyInfo.row = row;
-        $scope.keyInfo.col = col;
-
-        // 车辆信息
-        var url = _host.api_url + "/user/" + userId + "/car?active=1&carId=" + id;
-
-        _basic.get(url).then(function (data) {
-            if (data.success == true) {
-                if (data.result.length > 0) {
-                    // 初期化数据
-
-
-
-                    $('.modal').modal();
-                    $('#carKeyInfo').modal('open');
-                } else {
-                    swal("该钥匙没有对应的车辆信息！", "", "warning");
-                }
-            } else {
-                swal(data.msg, "", "error");
-            }
-        });
+    // 支付信息
+    $scope.paymentInfo = {
+        // 订单ID
+        id: 0,
+        // 订单ID
+        storageOrderIds: 0,
+        // 委托人ID
+        entrustId: 0,
+        // 支付方式
+        paymentType: "",
+        // 编号
+        number: "",
+        // 支付金额(美元)
+        paymentMoney: "",
+        // 支付描述
+        remark: "",
+        // 操作员
+        paymentUserName: "",
+        // 支付时间
+        paymentEndDate:""
     };
 
     /**
@@ -91,35 +80,49 @@ app.controller("storage_order_detail_controller", ["$scope", "$state", "$statePa
     $scope.openAssociatedOrder = function () {
         $('.modal').modal();
         $('#associatedOrderInfoDiv').modal('open');
-        queryOrderData();
     };
 
 
     $scope.openOtherOrderList = function () {
         $scope.otherOrderList = true;
+        // 取得关联其他订单数据。
+        getOtherOrderPayment();
     };
 
     /**
      * 打开修改价格模态窗口。
      */
-    $scope.openChangePriceDiv = function (el) {
+    $scope.openChangePriceDiv = function () {
         $('.modal').modal();
         $('#changePriceDiv').modal('open');
+    };
 
-        $scope.orderInfo.id = el.id;
-        // 委托方
-        $scope.orderInfo.entrustName = el.entrust_name;
-        // VIN
-        $scope.orderInfo.vin = el.vin;
-        // 车型
-        $scope.orderInfo.makeName = el.make_name;
-        $scope.orderInfo.modelName = el.model_name;
-        // 入库时间
-        $scope.orderInfo.enterTime = el.enter_time;
-        // 出库时间
-        $scope.orderInfo.realOutTime = el.real_out_time;
-        // 预计支付
-        $scope.orderInfo.planFee = el.plan_fee;
+    /**
+     * 修改价格，并刷新画面。
+     */
+    $scope.updateOrder = function () {
+
+        if ($scope.orderInfo.actualFee !== "") {
+            // 修改画面数据
+            var obj = {
+                actualFee: $scope.orderInfo.actualFee
+            };
+
+            var url = _host.api_url + "/user/" + userId + "/storageOrder/" + $scope.storageOrderId;
+
+            console.log(url);
+
+            _basic.put(url, obj).then(function (data) {
+                if (data.success) {
+                    $('#changePriceDiv').modal('close');
+                    swal("修改成功", "", "success");
+                } else {
+                    swal(data.msg, "", "error");
+                }
+            })
+        } else {
+            swal("请填写实际应付价格！", "", "warning");
+        }
     };
 
     /**
@@ -129,26 +132,41 @@ app.controller("storage_order_detail_controller", ["$scope", "$state", "$statePa
         $('.modal').modal();
         $('#paymentInfoDiv').modal('open');
         $scope.otherOrderList = false;
+
+        // 委托人ID
+        $scope.paymentInfo.entrustId = $scope.orderInfo.entrustId;
+        // 订单ID
+        $scope.paymentInfo.storageOrderIds = $scope.storageOrderId;
     };
 
     /**
-     * 根据画面输入的查询条件，进行数据查询。
+     * 执行[支付]操作。
      */
-    function queryOrderData() {
-        // 检索用url
-        var reqUrl = _host.api_url + "/storageOrder?start=" + 0 + "&size=" + 11;
+    $scope.orderPayment = function () {
 
-        console.log(reqUrl);
+        if ($scope.paymentInfo.paymentType !== "" && $scope.paymentInfo.number !== "" && $scope.paymentInfo.paymentMoney !== "" ) {
 
-        _basic.get(reqUrl).then(function (data) {
-            if (data.success == true) {
-                $scope.orderResult = data.result;
-                $scope.orderList = $scope.orderResult.slice(0, 10);
-            } else {
-                swal(data.msg, "", "error");
-            }
-        });
-    }
+            // 修改订单状态为【2：已支付】
+            var url = _host.api_url + "/user/" + userId + "/storageOrder/" + $scope.storageOrderId + "/orderStatus/" + $scope.payStatusList[1].id;
+
+            _basic.put(url, {}).then(function (data) {
+                if (data.success) {
+                    _basic.post(_host.api_url + "/user/" + userId + "/payment", $scope.paymentInfo).then(function (data) {
+                        if (data.success) {
+                            $('#paymentInfoDiv').modal('close');
+                            swal("支付成功", "", "success");
+                        } else {
+                            swal(data.msg, "", "error");
+                        }
+                    })
+                } else {
+                    swal(data.msg, "", "error");
+                }
+            });
+        } else {
+            swal("请填写完整信息！", "", "warning");
+        }
+    };
 
     // /**
     //  * 获取钥匙柜（分区）详细信息。
@@ -211,9 +229,19 @@ app.controller("storage_order_detail_controller", ["$scope", "$state", "$statePa
 
         _basic.get(url).then(function (data) {
             if (data.success == true) {
-                //
+
+                if (data.result.length == 0) {
+                    return;
+                }
+
+                // 订单是已支付时，取得支付详情
+                if (data.result[0].order_status == 2) {
+                    getPaymentDetails();
+                }
+                // 支付状态
                 $scope.orderInfo.orderStatus = data.result[0].order_status == null ? '0' : data.result[0].order_status;
                 // 委托方
+                $scope.orderInfo.entrustId = data.result[0].entrust_id == null ? 0 : data.result[0].entrust_id;
                 $scope.orderInfo.entrustName = data.result[0].entrust_name == null ? '未知' : data.result[0].entrust_name;
                 // vin
                 $scope.orderInfo.vin = data.result[0].vin == null ? '未知' : data.result[0].vin;
@@ -243,6 +271,129 @@ app.controller("storage_order_detail_controller", ["$scope", "$state", "$statePa
             }
         });
     }
+
+    /**
+     * 取得支付详情
+     */
+    function getPaymentDetails() {
+        // 检索用url
+        var url = _host.api_url + "/storageOrderPayment?storageOrderId=" + $scope.storageOrderId;
+
+        console.log(url);
+
+        _basic.get(url).then(function (data) {
+            if (data.success == true) {
+                // 支付编号
+                $scope.paymentInfo.id = data.result[0].id == null ? '' : data.result[0].id;
+                // 支付方式
+                $scope.paymentInfo.paymentType = data.result[0].payment_type == null ? '未知' : data.result[0].payment_type;
+                // 编号
+                $scope.paymentInfo.number = data.result[0].number == null ? 0 : data.result[0].number;
+                // 支付金额(美元)
+                $scope.paymentInfo.paymentMoney = data.result[0].payment_money == null ? '未知' : data.result[0].payment_money;
+                // 支付描述
+                $scope.paymentInfo.remark = data.result[0].remark == null ? '' : data.result[0].remark;
+                // 操作员
+                $scope.paymentInfo.paymentUserName = data.result[0].payment_user_name == null ? '未知' : data.result[0].payment_user_name;
+                // 支付时间
+                $scope.paymentInfo.paymentEndDate = data.result[0].payment_end_date;
+                // 查看关联的仓储订单
+                getOrderPaymentRel(data.result[0].id);
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    }
+
+    /**
+     * 查看关联的仓储订单
+     */
+    function getOrderPaymentRel(paymentId) {
+        // 检索用url
+        var url = _host.api_url + "/storageOrderPaymentRel?storageOrderPaymentId=" + paymentId;
+
+        console.log(url);
+
+        _basic.get(url).then(function (data) {
+            if (data.success == true) {
+                console.log(data.result);
+                $scope.relOrderList = data.result;
+
+                var totalPlanFee = 0;
+                var totalActualFee = 0;
+                data.result.reduce(function(previousValue, currentValue) {
+                    totalPlanFee =  previousValue.plan_fee + currentValue.plan_fee;
+                    totalActualFee = previousValue.actual_fee + currentValue.actual_fee;
+                });
+
+                $scope.totalPlanFee = totalPlanFee;
+                $scope.totalActualFee = totalActualFee;
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    }
+
+    /**
+     * 关联其他订单
+     */
+    function getOtherOrderPayment() {
+
+        // 查询状态为[1：未支付]的所有订单
+        var url = _host.api_url + "/storageOrder?orderStatus=" +  + $scope.payStatusList[0].id;;
+
+        console.log('----------------------------' + url);
+
+        _basic.get(url).then(function (data) {
+            if (data.success == true) {
+                console.log(data.result);
+                $scope.relOrderList = data.result;
+
+                var totalPlanFee = 0;
+                var totalActualFee = 0;
+                // $scope.relOrderList.reduce(function(previousValue, currentValue) {
+                //     console.log("previousValue.plan_fee is : " + previousValue.plan_fee);
+                //     console.log("previousValue.actual_fee is : " + previousValue.actual_fee);
+                //     totalPlanFee =  previousValue.plan_fee + currentValue.plan_fee;
+                //     totalActualFee = previousValue.actual_fee + currentValue.actual_fee;
+                // });
+
+                // $scope.totalPlanFee = totalPlanFee;
+                // $scope.totalActualFee = totalActualFee;
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    }
+
+    $scope.selected = [];
+   $scope.selectedTags = [];
+
+    var updateSelected = function(action,id,name){
+         if(action == 'add' && $scope.selected.indexOf(id) == -1){
+                    $scope.selected.push(id);
+                $scope.selectedTags.push(name);
+                 }
+             if(action == 'remove' && $scope.selected.indexOf(id)!=-1){
+                var idx = $scope.selected.indexOf(id);
+                     $scope.selected.splice(idx,1);
+                      $scope.selectedTags.splice(idx,1);
+               }
+           }
+
+    $scope.updateSelection = function($event, id){
+             var checkbox = $event.target;
+             console.log(checkbox);
+        console.log('id si : ' + id);
+
+               var action = (checkbox.checked?'add':'remove');
+             // updateSelected(action,id,checkbox.name);
+       }
+
+    $scope.isSelected = function(id){
+        console.log($scope.selected);
+               return $scope.selected.indexOf(id)>=0;
+        };
 
     /**
      * 画面初期显示时，用来获取画面必要信息的初期方法。
