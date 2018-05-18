9da4cfd6c7f64ec23691be07313386e984d33209
star920626 <jing.zhang@myxxjs.com>
2018-05-18 15:01:57 +0800

    增加信用证 查询 添加基本信息功能

--- web/js/controller/finance/credit_card_controller.js
+++ web/js/controller/finance/credit_card_controller.js
@@ -1,6 +1,238 @@
 /**
- * 主菜单：财务管理 -> 金融车辆 控制器
+ * 主菜单：财务管理 -> 信用证 控制器
  */
 app.controller("credit_card_controller", ["$scope", "$rootScope", "_host", "_basic", "_config", "$state", "$stateParams","$filter", function ($scope, $rootScope, _host, _basic, _config, $state, $stateParams,$filter) {
+    // 取得当前画面 登录用户
+    var userId = _basic.getSession(_basic.USER_ID);
+
+    // 翻页用
+    $scope.start = 0;
+    $scope.size = 11;
+
+    // 委托方性质
+    $scope.entrustTypeList = _config.entrustType;
+
+    // 状态
+    $scope.statusList = _config.paymentStatus;
+
+
+    /**
+     * 页面跳转
+     */
+    $scope.baseMsg = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.baseMsg ').addClass("active");
+        $("#baseMsg").addClass("active");
+        $("#baseMsg").show();
+    };
+    $scope.linkFinanceCar = function () {
+        $('ul.tabWrap li').removeClass("active");
+        $(".tab_box").removeClass("active");
+        $(".tab_box").hide();
+        $('ul.tabWrap li.linkFinanceCar ').addClass("active");
+        $("#linkFinanceCar").addClass("active");
+        $("#linkFinanceCar").show();
+    };
+
+    /**
+     * 获取委托方信息
+     * @param type 委托方类型
+     */
+    $scope.getEntrustInfo = function (type) {
+        var url = '';
+        if (type == undefined) {
+            url = _host.api_url + "/entrust";
+        } else {
+            url = _host.api_url + "/entrust?entrustType=" + type;
+        }
+        _basic.get(url).then(function (data) {
+            if (data.success == true) {
+                $scope.addEntrustList = data.result;
+                $('#addEntrustSelect').select2({
+                    placeholder: '委托方',
+                    containerCssClass: 'select2_dropdown',
+                    allowClear: true
+                });
+                $("#addEntrustSelect").val(null).trigger("change");
+            }
+        });
+    };
+
+
+    /**
+     * 查询页面获取委托方信息
+     */
+    function getEntrustInfo (){
+        _basic.get(_host.api_url + "/entrust").then(function (data) {
+            if (data.success == true) {
+                $scope.entrustList = data.result;
+                $('#entrustSelect').select2({
+                    placeholder: '委托方',
+                    containerCssClass: 'select2_dropdown',
+                    allowClear: true
+                });
+            }
+        })
+    }
+
+
+    /**
+     * 组装检索条件。
+     */
+    function makeConditions() {
+        var entrust = {};
+
+        if ($("#entrustSelect").val() == "") {
+            entrust = {id:"",text:""};
+        } else {
+            entrust = $("#entrustSelect").select2("data")[0] ;
+        }
+
+        var obj = {
+            financialCreditId: $scope.condCreditId,
+            entrustType: $scope.condVin,
+            entrustId: entrust.condLoan,
+            creditStatus: $scope.condStatus,
+            entrustId: entrust.id,
+            planReturnDateStart: $scope.expectedDateStart,
+            planReturnDateEnd: $scope.expectedDateEnd,
+            actualReturnDateStart: $scope.actualDateStart,
+            actualReturnDateEnd: $scope.actualDateEnd
+        };
+        return obj;
+    }
+
+
+    /**
+     * 根据画面输入的查询条件，进行数据查询。
+     */
+    function queryCreditCardList() {
+        var reqUrl = _host.api_url + "/financialCredit?start=" + $scope.start + "&size=" + $scope.size;
+        // 检索条件
+        var conditions = _basic.objToUrl(makeConditions());
+        // 检索URL
+        reqUrl = conditions.length > 0 ? reqUrl + "&" + conditions : reqUrl;
+        _basic.get(reqUrl).then(function (data) {
+            if (data.success == true) {
+                $scope.creditCardBoxArray = data.result;
+                $scope.creditCardList = $scope.creditCardBoxArray.slice(0, 10);
+                if ($scope.start > 0) {
+                    $("#pre").show();
+                }
+                else {
+                    $("#pre").hide();
+                }
+                if (data.result.length < $scope.size) {
+                    $("#next").hide();
+                }
+                else {
+                    $("#next").show();
+                }
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    }
+
+
+    /**
+     * 点击：查询按钮，进行数据查询
+     */
+    $scope.queryCreditCardInfo = function () {
+        // 默认第一页
+        $scope.start = 0;
+        queryCreditCardList();
+    };
+
+
+    /**
+     * 打开【新增信用证】模态画面。
+     */
+    $scope.openNewCreditCardDiv = function () {
+        $('.modal').modal();
+        $('#newCreditCardDiv').modal('open');
+        $scope.getEntrustInfo();
+        $scope.baseMsg();
+    };
+
+    /**
+     *提交【新增信用证】模态画面的基本信息。
+     */
+    $scope.addCreditCardInfo = function () {
+
+        // 委托方 下拉选中 内容
+        var entrust = $("#addEntrustSelect").select2("data")[0]; //单选
+
+        if (entrust.id !== "" && $scope.addCreditId !== "" && $scope.addCreditMoney !== "" && $scope.addActualMoney !== "") {
+
+
+            // 追加画面数据
+            var obj = {
+                creditNumber: $scope.addCreditMoney,
+                entrustId: entrust.id,
+                creditMoney: $scope.addCreditMoney,
+                actualMoney: $scope.addActualMoney,
+                planReturnDate: $scope.addPlanReturnDate,
+                actualReturnDate: $scope.addActualReturnDate,
+                receiveCardDate: $scope.addReceiveCardDate,
+                documentsDate: $scope.addDocumentsDate,
+                documentsSendDate:$scope.addDocumentsSendDate,
+                documentsReceiveDate: $scope.addDocumentsReceiveDate,
+                actualRemitDate:$scope.addActualRemitDate,
+                invoiceNumber: $scope.addInvoiceNumber,
+                remark: $scope.addRemarkText
+            };
+
+
+            _basic.post(_host.api_url + "/user/" + userId + "/financialCredit", obj).then(function (data) {
+                if (data.success) {
+                    $scope.linkFinanceCar();
+                    /*$('#addPaymentModal').modal('close');
+                    queryCreditCardList();
+                    swal("新增成功", "", "success");*/
+
+                } else {
+                    swal(data.msg, "", "error");
+                }
+            })
+        } else {
+            swal("请输入完整信息！", "", "warning");
+        }
+    };
+
+
+    /**
+     * 上一页
+     */
+    $scope.preBtn = function ()  {
+        $scope.start = $scope.start - ($scope.size - 1);
+        queryCreditCardList();
+    };
+
+    /**
+     * 下一页
+     */
+    $scope.nextBtn = function () {
+        $scope.start = $scope.start + ($scope.size - 1);
+        queryCreditCardList();
+    };
+
+
+
+    /**
+     * 画面初期显示时，用来获取画面必要信息的初期方法。
+     */
+    $scope.initData = function () {
+        // 查询数据
+        queryCreditCardList();
+        // 获取委托方信息
+        getEntrustInfo();
+    };
+    $scope.initData();
+
+
+
 
 }])
