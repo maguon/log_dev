c2e3e954d830e94ac2367bb943f4007d4c62e7b8
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-06-07 09:04:36 +0800

    修改：新规/修改 使用同一代码段。统一代码风格

--- web/js/controller/setting/setting_port_controller.js
+++ web/js/controller/setting/setting_port_controller.js
@@ -2,109 +2,153 @@
  * Created by star on 2018/4/11.
  */
 app.controller("setting_port_controller", ["$scope", "_basic", "_config", "_host", function ($scope, _basic, _config, _host) {
+
+    // 取得当前画面 登录用户
     var userId = _basic.getSession(_basic.USER_ID);
-    $scope.countryList = _config.country;//获取国家名字
-    /**
-     * 获取港口名字
-     * */
-    function getPortName() {
-        // 港口
-        _basic.get(_host.api_url + "/port").then(function (data) {
-            if (data.success == true) {
-                $scope.portList = data.result;
-            }
-        });
+
+    // 获取国家名称列表
+    $scope.countryList = _config.country;
+
+    // 追加画面初期数据
+    var initPortInfo = {
+        // 国家
+        country:"",
+        // 港口名称
+        portName:"",
+        // 描述
+        remark:""
     };
+
+    // 追加画面数据
+    $scope.portInfo = {};
+
     /**
-     *  搜索
-     *  */
+     * 检索港口
+     */
     $scope.searchPort = function () {
         var obj = {
-            portId: $scope.portName,
-            countryId: $scope.countryName
+            portId: $scope.condPortName,
+            countryId: $scope.condCountryName
         };
         _basic.get(_host.api_url + "/port?" + _basic.objToUrl(obj)).then(function (data) {
-            if (data.success == true) {
+            if (data.success) {
                 $scope.portArr = data.result;
             }
         })
     };
+
     /**
-    * 添加模态框
-    * */
-    $scope.addPort = function (){
-        $(".modal").modal();
-        $("#addPort").modal("open");
-        $scope.addCountry="";
-        $scope.addPortName="";
-        $scope.addRemark="";
-    }
-    $scope.addPortItem = function (){
-        if ($scope.addCountry !== ""&& $scope.addPortName !== "") {
+     * 打开画面【增加港口】模态框。
+     */
+    $scope.openAddPort = function (){
+        // 画面ID
+        $scope.pageId = "add";
+        // 初期化数据
+        angular.copy(initPortInfo, $scope.portInfo);
 
-            // 追加画面数据
-            var obj = {
-                portName:$scope.addPortName,
-                countryId:$scope.addCountry,
-                address:"",
-                remark: $scope.addRemark
-            };
+        // 打开 模态窗口
+        $('.modal').modal();
+        $('#savePortDiv').modal('open');
 
-            // 调用 API
-            _basic.post(_host.api_url + "/user/" + userId + "/port", obj).then(function (data) {
-                if (data.success) {
-                    $('#addPort').modal('close');
-                    swal("新增成功", "", "success");
-                    // 成功后，刷新页面数据
-                    $scope.searchPort();
-                } else {
-                    swal(data.msg, "", "error");
+        // textarea 高度调整
+        $('#remark').val('');
+        $('#remark').trigger('autoresize');
+    };
+
+    /**
+     * 打开画面【港口信息】模态框。
+     * @param selectedItem 选中数据
+     */
+    $scope.openEditPort = function (selectedItem){
+        // 根据画面选中数据的ID 检索数据
+        _basic.get(_host.api_url + "/port?portId=" + selectedItem.id).then(function (data) {
+            if (data.success) {
+                if (data.result.length > 0) {
+                    // 画面ID
+                    $scope.pageId = "edit";
+
+                    $(".modal").modal();
+                    $("#savePortDiv").modal("open");
+
+                    // 国家
+                    $scope.portInfo.country = data.result[0].country_id;
+                    // 港口名称
+                    $scope.portInfo.portName = data.result[0].port_name;
+                    // 描述
+                    $scope.portInfo.remark = data.result[0].remark;
+
+                    // textarea 高度调整
+                    $('#remark').val($scope.portInfo.remark);
+                    $('#remark').trigger('autoresize');
                 }
-            })
-        } else {
-            swal("请填写完整信息！", "", "warning");
-        }
-    }
-    /**
-     * 修改模态框
-     * */
-    $scope.updatePort = function (id){
-        $(".modal").modal();
-        $("#updatePort").modal("open");
-        $scope.id=id;
-        _basic.get(_host.api_url + "/port?portId="+id).then(function (data) {
-            if (data.success == true) {
-                $scope.updateCountry = data.result[0].country_id;
-                $scope.updatePortName = data.result[0].port_name;
-                $scope.updateRemark = data.result[0].remark;
             }
         })
     };
-    $scope.updatePortItem = function (){
-        if($scope.updateCountry !== ""&& $scope.updatePortName !== ""){
+
+    $scope.savePortInfo = function (){
+        if ($scope.portInfo.country !== "" && $scope.portInfo.portName !== "") {
+
+            // 追加画面数据
             var obj = {
-                portName: $scope.updatePortName,
-                countryId:$scope.updateCountry,
+                portName:$scope.portInfo.portName,
+                countryId:$scope.portInfo.country,
                 address:"",
-                remark: $scope.updateRemark
+                remark: $scope.portInfo.remark
             };
-            _basic.put(_host.api_url + "/user/" + userId+"/port/" + $scope.id, obj).then(function (data) {
-                if (data.success == true) {
-                    swal("修改成功", "", "success");
-                    $('#updatePort').modal('close');
-                    $scope.searchPort();
-                } else {
-                    swal(data.msg, "", "error");
-                }
-            })
-        }
-        else{
+
+            if ($scope.pageId === "add") {
+                // 调用 API
+                _basic.post(_host.api_url + "/user/" + userId + "/port", obj).then(function (data) {
+                    if (data.success) {
+                        $('#savePortDiv').modal('close');
+                        swal("新增成功", "", "success");
+                        // 成功后，刷新页面数据
+                        $scope.searchPort();
+                    } else {
+                        swal(data.msg, "", "error");
+                    }
+                })
+            } else {
+                _basic.put(_host.api_url + "/user/" + userId+"/port/" + $scope.id, obj).then(function (data) {
+                    if (data.success) {
+                        $('#savePortDiv').modal('close');
+                        swal("修改成功", "", "success");
+                        // 成功后，刷新页面数据
+                        $scope.searchPort();
+                    } else {
+                        swal(data.msg, "", "error");
+                    }
+                })
+            }
+        } else {
             swal("请填写完整信息！", "", "warning");
         }
-    }
+    };
+
     /**
-     * *数据获取
-     * */
-    getPortName();
-    $scope.searchPort();
+     * 获取港口名称列表
+     */
+    function getPortList() {
+        // 港口
+        _basic.get(_host.api_url + "/port").then(function (data) {
+            if (data.success) {
+                $scope.portList = data.result;
+            }
+        });
+    }
+
+    /**
+     * 初期检索画面数据
+     */
+    function initData() {
+        // 获取港口名称列表
+        getPortList();
+        // 检索港口
+        $scope.searchPort();
+    }
+
+    /**
+     * 画面初期数据取得。
+     */
+    initData();
 }]);
