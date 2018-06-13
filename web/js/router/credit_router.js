08ab3d6b109632b2968edefad65aaf612f41ed08
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-06-13 09:22:30 +0800

    修改：信用证管理员，追加 财务主控面板 以及 财务管理-信用证管理。

--- web/js/router/credit_router.js
+++ web/js/router/credit_router.js
@@ -2,8 +2,14 @@
  * 信用证管理员-使用画面 路由配置.
  */
 app.config(['$stateProvider',"$urlRouterProvider",function($stateProvider,$urlRouterProvider) {
-    $urlRouterProvider.when("","/car_demand");
+    $urlRouterProvider.when("","/finance_index");
     $stateProvider
+        // 主菜单：主控面板(财务管理员)
+        .state("finance_index", {
+            url: "/finance_index",
+            templateUrl: "/view/finance/finance_index.html",
+            controller:'finance_index_controller'
+        })
 
         // 主菜单：车辆查询 画面
         .state("car_demand",{
@@ -16,6 +22,20 @@ app.config(['$stateProvider',"$urlRouterProvider",function($stateProvider,$urlRo
             url:"/car_demand_details/{id}/vin/{vin}?from",
             templateUrl: "/view/car_demand/car_demand_details.html",
             controller:'car_demand_details_controller'
+        })
+
+        // 财务管理->信用证
+        .state("credit_card", {
+            url:"/credit_card",
+            params: {"from": null},
+            templateUrl: "/view/finance/credit_card.html",
+            controller:"credit_card_controller"
+        })
+        .state("credit_card_detail", {
+            url:"/credit_card_detail/{id}?from",
+            params: {"from": null},
+            templateUrl: "/view/finance/credit_card_detail.html",
+            controller:"credit_card_detail_controller"
         })
     ;
 }]);
