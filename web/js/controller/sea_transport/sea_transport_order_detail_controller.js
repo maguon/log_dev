00e24e3ef9fca08d9e503a3911dc231320f45132
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-07-30 13:47:01 +0800

    修改：追加 【海运费用】编辑显示画面

--- web/js/controller/sea_transport/sea_transport_order_detail_controller.js
+++ web/js/controller/sea_transport/sea_transport_order_detail_controller.js
@@ -6,10 +6,32 @@ app.controller("sea_transport_order_detail_controller", ["$scope","$stateParams"
     // 用户ID
     var userId = _basic.getSession(_basic.USER_ID);
 
-    // 订单 ID
-    $scope.seaTransportOrderId = $stateParams.id;
+    // 海运订单 ID
+    var shipTransOrderId = $stateParams.id;
 
+    // 支付状态
     $scope.payStatus = _config.payStatus;
+
+    // 海运费用类别
+    $scope.shipTransFeeTypes = _config.shipTransFeeTypes;
+
+    // 新增 海运费用 画面默认数据
+    var defaultShipTransFee = {
+        // 前画面订单ID
+        shipTransOrderId: "",
+        // 画面区分
+        pageType: "",
+        // 选择付费项目
+        type: "",
+        // 金额(美元)
+        fee: "",
+        // 合计费用(美元)
+        totalFee: 0
+    };
+
+    // 新增 海运费用 画面用
+    $scope.newShipTransFee = {};
+
     /**
      * 返回到前画面（订单管理）。
      */
@@ -17,12 +39,11 @@ app.controller("sea_transport_order_detail_controller", ["$scope","$stateParams"
         $state.go($stateParams.from, {}, {reload: true})
     };
 
-
     /**
      * 根据id获得详情。
      */
     function queryOrderData() {
-        _basic.get( _host.api_url + "/shipTransOrder?shipTransOrderId="+$scope.seaTransportOrderId).then(function (data) {
+        _basic.get( _host.api_url + "/shipTransOrder?shipTransOrderId=" + shipTransOrderId).then(function (data) {
             if (data.success) {
                 if(data.result.length===0){
                     return;
@@ -35,14 +56,13 @@ app.controller("sea_transport_order_detail_controller", ["$scope","$stateParams"
         });
     }
 
-
     /*
     * 获取支付信息
     * */
-    function getPaymentInfo(id){
-        _basic.get( _host.api_url + "/payment?shipTransOrderId="+id).then(function (data) {
+    function getPaymentInfo(id) {
+        _basic.get(_host.api_url + "/payment?shipTransOrderId=" + id).then(function (data) {
             if (data.success) {
-                if(data.result.length===0){
+                if (data.result.length === 0) {
                     return;
                 }
                 $scope.paymentList = data.result[0];
@@ -52,20 +72,158 @@ app.controller("sea_transport_order_detail_controller", ["$scope","$stateParams"
         });
     }
 
-    /*
-    * 修改运费
-    * */
-    $scope.changeShipTransFee = function (id){
-        var obj = {
-            shipTransFee:$scope.paymentInfo.total_fee
-        };
-        _basic.put(_host.api_url + "/user/" + userId + "/shipTransOrder/"+id+'/ShipTransOrderFee', obj).then(function (data) {
+    /**
+     * 打开追加【海运费用】模态画面。
+     * @param pageType 画面区分(新建/编辑)
+     */
+    $scope.showEditShipFeeDiv = function (pageType) {
+        $('.modal').modal();
+        $('#editShipFeeDiv').modal('open');
+
+        // 初期化数据
+        angular.copy(defaultShipTransFee, $scope.newShipTransFee);
+        $scope.newShipTransFee.shipTransOrderId = shipTransOrderId;
+        $scope.newShipTransFee.pageType = pageType;
+
+        // 取得海运费用一览
+        getShipTransOrderFeeRel();
+    };
+
+    /**
+     * 取得海运费用一览
+     */
+    function getShipTransOrderFeeRel() {
+        // 检索用url
+        var url = _host.api_url + "/shipTransOrderFeeRel?shipTransOrderId=" + shipTransOrderId;
+
+        // 合计费用(美元)
+        $scope.newShipTransFee.totalFee = 0;
+        var thisFee = 0;
+        _basic.get(url).then(function (data) {
             if (data.success) {
-                swal('修改成功!', "", "success");
+                $scope.shipTransOrderFeeRelList = data.result;
+                // 计算 合计费用(美元)
+                for (var i = 0; i < $scope.shipTransOrderFeeRelList.length; i++) {
+                    thisFee = $scope.shipTransOrderFeeRelList[i].pay_money;
+                    $scope.newShipTransFee.totalFee = $scope.newShipTransFee.totalFee + thisFee;
+                }
             } else {
                 swal(data.msg, "", "error");
             }
         });
+    }
+
+    /**
+     * 追加海运费用 （画面加号按钮）
+     */
+    $scope.addShipTransOrderFeeRel = function () {
+        if ($scope.newShipTransFee.type !== "" && $scope.newShipTransFee.fee !== "") {
+            var obj = {
+                payType: $scope.newShipTransFee.type,
+                payMoney: $scope.newShipTransFee.fee
+            };
+
+            _basic.post(_host.api_url + "/user/" + userId + "/shipTransOrder/" + $scope.newShipTransFee.shipTransOrderId + "/shipTransOrderFeeRel", obj).then(function (data) {
+                if (data.success) {
+                    swal("追加成功", "", "success");
+
+                    // 初期化数据
+                    $scope.newShipTransFee.type = "";
+                    $scope.newShipTransFee.fee = "";
+
+                    // 取得海运费用一览
+                    getShipTransOrderFeeRel();
+                    // 取得运载车辆详情 （画面下部分）
+                    queryOrderData();
+                } else {
+                    swal(data.msg, "", "error");
+                }
+            })
+        } else {
+            swal("请填写完整海运费用信息！", "", "warning");
+        }
+    };
+
+    /**
+     * 从列表中，修改指定海运费用
+     *
+     * @param shipTransFeeInfo 海运费用信息
+     */
+    $scope.updShipTransOrderFeeRel = function (shipTransFeeInfo) {
+        if (shipTransFeeInfo.pay_money !== "") {
+            swal({
+                    title: "",
+                    text: "确认修改？",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    closeOnConfirm: false,
+                    closeOnCancel: false
+                },
+                function (isConfirm) {
+                    if (isConfirm) {
+                        var obj = {
+                            payMoney: shipTransFeeInfo.pay_money
+                        };
+                        // 修改费用
+                        var url = _host.api_url + "/user/" + userId + "/shipTransOrderFeeRel/" + shipTransFeeInfo.id;
+                        _basic.put(url, obj).then(function (data) {
+                            if (data.success) {
+                                swal("修改费用成功", "", "success");
+                                // 取得海运费用一览
+                                getShipTransOrderFeeRel();
+                                // 取得运载车辆详情 （画面下部分）
+                                queryOrderData();
+                            } else {
+                                swal(data.msg, "", "error");
+                            }
+                        })
+                    } else {
+                        swal.close();
+                    }
+                });
+        } else {
+            swal("请填写费用金额！", "", "warning");
+        }
+    };
+
+    /**
+     * 从列表中，删除指定海运费用
+     *
+     * @param shipTransOrderFeeRelId 删除费用ID
+     */
+    $scope.delShipTransOrderFeeRel = function (shipTransOrderFeeRelId) {
+        swal({
+                title: "",
+                text: "确认删除？",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "确定",
+                cancelButtonText: "取消",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },
+            function (isConfirm) {
+                if (isConfirm) {
+                    _basic.delete(_host.api_url + "/user/" + userId + "/shipTransOrderFeeRel/" + shipTransOrderFeeRelId, {}).then(
+                        function (data) {
+                            if (data.success) {
+                                swal("删除费用成功", "", "success");
+                                // 取得海运费用一览
+                                getShipTransOrderFeeRel();
+                                // 取得运载车辆详情 （画面下部分）
+                                queryOrderData();
+                            } else {
+                                swal(data.msg, "", "error");
+                            }
+                        });
+                } else {
+                    swal.close();
+                }
+            });
     };
 
     /**
