b112983001905d60d6600ca95fbaeaa4405f9b92
yjyvsdream <jingyuan.yu@myxxjs.com>
2018-08-02 14:58:18 +0800

    修改：追加 费用明细

--- web/js/controller/finance/payment_detail_controller.js
+++ web/js/controller/finance/payment_detail_controller.js
@@ -11,6 +11,9 @@ app.controller("payment_detail_controller", ["$scope", "$stateParams", "_basic",
     $scope.paymentStatusList = _config.paymentStatus;
     // 支付方式
     $scope.paymentTypeList = _config.paymentType;
+    // 海运费用类别
+    $scope.shipTransFeeTypes = _config.shipTransFeeTypes;
+
     // 关联仓储订单 合计应付
     $scope.totalMoney = 0;
     // 关联海运订单 合计应付
@@ -492,6 +495,49 @@ app.controller("payment_detail_controller", ["$scope", "$stateParams", "_basic",
             }
         });
     };
+
+    /**
+     * 打开追加【费用明细】模态画面。
+     * @param shipTransOrderId 海运订单编号
+     * @param vin vin
+     */
+    $scope.showShipFeeDetailDiv = function (shipTransOrderId, vin) {
+        $('.modal').modal();
+        $('#shipFeeDetailDiv').modal('open');
+
+        $scope.shipTransFee = {};
+        // 海运订单编号
+        $scope.shipTransFee.shipTransOrderId = shipTransOrderId;
+        $scope.shipTransFee.vin = vin;
+
+        // 取得海运费用一览
+        getShipTransOrderFeeRel(shipTransOrderId);
+    };
+
+    /**
+     * 取得海运费用一览
+     * @param shipTransOrderId 海运订单编号
+     */
+    function getShipTransOrderFeeRel(shipTransOrderId) {
+        // 检索用url
+        var url = _host.api_url + "/shipTransOrderFeeRel?shipTransOrderId=" + shipTransOrderId;
+
+        // 合计费用(美元)
+        $scope.shipTransFee.totalFee = 0;
+        var thisFee = 0;
+        _basic.get(url).then(function (data) {
+            if (data.success) {
+                $scope.shipTransOrderFeeRelList = data.result;
+                // 计算 合计费用(美元)
+                for (var i = 0; i < $scope.shipTransOrderFeeRelList.length; i++) {
+                    thisFee = $scope.shipTransOrderFeeRelList[i].pay_money;
+                    $scope.shipTransFee.totalFee = $scope.shipTransFee.totalFee + thisFee;
+                }
+            } else {
+                swal(data.msg, "", "error");
+            }
+        });
+    }
 
     /**
      * 获取基本信息
