3b4f65be01fb1ba5adc81021e9c7c6e0b319505e
yjyvsdream <jingyuan.yu@myxxjs.com>
2019-04-19 13:44:49 +0800

    修改：车辆信息 画面 追加 PDF 预览 下载功能

--- web/js/controller/car_demand/car_demand_details_controller.js
+++ web/js/controller/car_demand/car_demand_details_controller.js
@@ -1,11 +1,13 @@
 /**
- * Created by ASUS on 2017/5/5.
  * 主菜单：车辆查询 -> 车辆信息詳情 画面
  */
 app.controller("car_demand_details_controller", ["$state", "$stateParams", "_config", "$scope", "_host", "_basic", function ($state, $stateParams, _config, $scope, _host, _basic) {
     var userId = _basic.getSession(_basic.USER_ID);
-    // 通过url中的信息取得 车辆id 和 VIN码
+    // 当前时间
+    var nowStr = moment(new Date()).format("YYYYMMDD_HH:mm:ss.SSS");
+    // 通过url中的信息取得 车辆id
     var val = $stateParams.id;
+    // 通过url中的信息取得 VIN码
     var vin = $stateParams.vin;
     // 一行一列内，多个停车位区分用 (A-Z)
     $scope.characters = _config.characters;
@@ -17,6 +19,14 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
     $scope.shipTransStatus = _config.shipTransStatus;
     // 是否MSO车辆 列表
     $scope.msoFlags = _config.msoFlags;
+    // 发票开具-公司信息
+    $scope.companyInfo = _config.companyInfo;
+    // PDF预览 - 车辆基本信息
+    $scope.showCarInfo4PDF = true;
+    // PDF预览 - 仓储信息
+    $scope.showStorageInfo4PDF = true;
+    // PDF预览 - 海运信息
+    $scope.showTransInfo4PDF = true;
 
     /**
      * 返回到前画面（车辆查询）。
@@ -54,24 +64,99 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
     };
 
     /**
-     * 点击 发票预览
+     * 更新是否显示 车辆基本信息 标记
+     * @param $event
      */
-    $scope.previewInvoice = function () {
-        $('.modal').modal();
-        $('#carInfoDiv').modal('open');
+    $scope.changeShowCarInfoFlg = function ($event) {
+        // 车辆基本信息 选中的情况
+        $scope.showCarInfo4PDF = $event.target.checked;
     };
 
     /**
-     * 进行canvas生成。(点击下载按钮)
+     * 更新是否显示 仓储信息 标记
+     * @param $event
      */
-    $scope.downloadInvoice = function () {
+    $scope.changeShowStorageInfoFlg = function ($event) {
+        // 仓储信息 选中的情况
+        $scope.showStorageInfo4PDF = $event.target.checked;
+    };
+
+    /**
+     * 更新是否显示 海运信息 标记
+     * @param $event
+     */
+    $scope.changeShowTransInfoFlg = function ($event) {
+        // 海运信息 选中的情况
+        $scope.showTransInfo4PDF = $event.target.checked;
+    };
+
+    /**
+     * 点击 PDF预览
+     */
+    $scope.previewPDF = function () {
+        $('.modal').modal();
+        $('#carInfoDiv').modal('open');
+        getPDFCarImage();
+    };
+
+    /**
+     * 获取各类别车辆照片
+     */
+    function getPDFCarImage() {
+        // 车辆照片
+        $scope.pdf_car_imageBox = [];
+        // 仓储照片
+        $scope.pdf_storage_imageBox = [];
+        // 海运照片
+        $scope.pdf_trans_imageBox = [];
+
+        //图片获取
+        _basic.get(_host.record_url + "/user/" + userId + "/car/" + val + "/record").then(function (data) {
+            if (data.success) {
+                if (data.result.length > 0) {
+                    // 选中 车辆基本信息
+                    if ($scope.showCarInfo4PDF) {
+                        $scope.car_image = data.result[0].car_image;
+                        for (var i in $scope.car_image) {
+                            $scope.pdf_car_imageBox.push({
+                                src: _host.file_url + '/image/' + $scope.car_image[i].url + "?" + nowStr
+                            });
+                        }
+                    }
+                    // 选中 仓储信息
+                    if ($scope.showStorageInfo4PDF) {
+                        $scope.storage_image = data.result[0].storage_image;
+                        for (var i in $scope.storage_image) {
+                            $scope.pdf_storage_imageBox.push({
+                                src: _host.file_url + '/image/' + $scope.storage_image[i].url + "?" + nowStr
+                            });
+                        }
+                    }
+                    // 选中 海运信息
+                    if ($scope.showTransInfo4PDF) {
+                        $scope.trans_image = data.result[0].trans_image;
+                        for (var i in $scope.trans_image) {
+                            $scope.pdf_trans_imageBox.push({
+                                src: _host.file_url + '/image/' + $scope.trans_image[i].url + "?" + nowStr
+                            });
+                        }
+                    }
+                }
+            } else {
+                swal(data.msg, "", "error")
+            }
+        });
+    }
+
+    /**
+     * 下载PDF文件。
+     */
+    $scope.downloadPDF = function () {
         try{
             // 去掉画面CSS (主要为了去滚动条)
             $("#context-div").removeClass("ConWrap");
             $("#carInfoDiv").removeClass("modal");
-            // html2canvas(document.getElementById('recommend-div'),{useCORS: true}).then((canvas) => {
-            //
-            // }
+            $(".shadeDowWrap").show();
             html2canvas(document.getElementById("car_info"), {
                 // allowTaint: true, //避免一些不识别的图片干扰，默认为false，遇到不识别的图片干扰则会停止处理html2canvas
                 // taintTest: false,
@@ -83,42 +168,49 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
                 onrendered: function(canvas) {
                     var contentWidth = canvas.width;
                     var contentHeight = canvas.height;
+                    console.log('contentWidth',contentWidth);
+                    console.log('contentHeight',contentHeight);
 
                     //一页pdf显示html页面生成的canvas高度;
                     var pageHeight = contentWidth / 595.28 * 841.89;
+                    console.log('pageHeight',pageHeight);
                     //未生成pdf的html页面高度
                     var leftHeight = contentHeight;
+                    console.log('leftHeight',leftHeight);
                     //pdf页面偏移
                     var position = 0;
                     //a4纸的尺寸[595.28,841.89]，html页面生成的canvas在pdf中图片的宽高
                     var imgWidth = 595.28;
                     var imgHeight = 595.28/contentWidth * contentHeight;
+                    console.log('imgHeight',imgHeight);
                     var pageData = canvas.toDataURL('image/jpeg', 1.0);
 
-                    // pageData = pageData.replace(/^data:image\/(png|jpg);base64,/, "");
-                    //
-                    // saveFile(pageData,'aaa.png');
+                    var pdf = new jsPDF('', 'pt', [595.28, contentHeight]);
+                    // pdf.beginFormObject(10, 10, 100, 100, matrix);
+                    pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
 
-                    var pdf = new jsPDF('', 'pt', 'a4');
+
                     //有两个高度需要区分，一个是html页面的实际高度，和生成pdf的页面高度(841.89)
                     //当内容未超过pdf一页显示的范围，无需分页
-                    if (leftHeight < pageHeight) {
-                        pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
-                    } else {
-                        while(leftHeight > 0) {
-                            pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
-                            leftHeight -= pageHeight;
-                            position -= 841.89;
-                            //避免添加空白页
-                            if(leftHeight > 0) {
-                                pdf.addPage();
-                            }
-                        }
-                    }
-                    // 当前 日期
-                    // var now = moment(new Date()).format('YYYY-MM-DD');
-                    // pdf.save('invoice_' + now + '.pdf');
-                    pdf.save('car_info.pdf');
+                    // if (leftHeight < pageHeight) {
+                    //     pdf.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight );
+                    // } else {
+                    //     while(leftHeight > 0) {
+                    //         pdf.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight);
+                    //         leftHeight -= pageHeight;
+                    //         console.log('leftHeight',leftHeight);
+                    //         // 比841.89 多减去20 ，是为了留下一定的余白
+                    //         position -= 861.89;
+                    //         console.log('position',position);
+                    //         //避免添加空白页
+                    //         if(leftHeight > 0) {
+                    //             pdf.addPage();
+                    //         }
+                    //     }
+                    // }
+                    // 保存PDF文件
+                    pdf.save(vin + '.pdf');
+                    $(".shadeDowWrap").hide();
                 },
                 // 背景设为白色（默认为黑色）
                 background: "#fff"
@@ -133,26 +225,9 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
     };
 
     /**
-     * 在本地进行文件保存
-     * @param  {String} data     要保存到本地的图片数据
-     * @param  {String} filename 文件名
-     */
-    function saveFile(data, filename) {
-        var save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
-        save_link.href = data;
-        save_link.download = filename;
-
-        var event = document.createEvent('MouseEvents');
-        event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-        save_link.dispatchEvent(event);
-    }
-
-
-    var viewer;
-
-    /**
      * 显示车辆照片大图。
      */
+    var viewer;
     $scope.showImgByViewer = function (type) {
         if (type === 'carImage') {
             viewer = new Viewer(document.getElementById('lookCarImage'), {
@@ -202,8 +277,6 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
                                 time:$scope.storage_image[i].timez,
                                 user:$scope.storage_image[i].name
                             });
-
-                            console.log('',$scope.storage_imageBox);
                         }
                     } else if (type === 'transImage') {
                         $scope.trans_image = data.result[0].trans_image;
@@ -223,12 +296,9 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
     };
 
     /**
-     * 通过车辆ID，取得仓储车辆信息。
-     *
-     * @param val 车辆ID
-     * @param vin VIN码
+     * 画面初期显示时，用来获取画面必要信息的初期方法。
      */
-    $scope.getStorageCarInfo = function (val, vin) {
+    function initData() {
         $(".main_storage_car").hide();
         $("#look_StorageCar").show();
         $('ul.tabWrap li').removeClass("active");
@@ -240,11 +310,8 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
 
         // 基本信息获取
         _basic.get(_host.api_url + "/user/" + userId + "/car?carId=" + val).then(function (data) {
-            if (data.success == true ) {
-                if (data.result.length == 0) {
-                    return;
-                }
-                else {
+            if (data.success) {
+                if (data.result.length > 0) {
                     $scope.modelId = data.result[0].model_id;
                     $scope.self_car = data.result[0];
                     $scope.carColor = '未知';
@@ -267,10 +334,9 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
                     $scope.look_car_id = $scope.self_car.id;
                 }
             }
-                else {
+            else {
                 swal(data.msg, "", "error")
             }
-
         });
 
         // 取得操作记录
@@ -281,6 +347,6 @@ app.controller("car_demand_details_controller", ["$state", "$stateParams", "_con
                 }
             }
         });
-    };
-    $scope.getStorageCarInfo(val, vin);
+    }
+    initData();
 }]);
